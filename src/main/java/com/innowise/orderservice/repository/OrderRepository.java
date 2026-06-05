8e1be5be97a715e9ac24dd2fe10d29041c1fb9c5
lefreshz <scver1@yandex.ru>
2026-06-05 23:21:08 +0300

    Add skeletons for repository classes and specification for orders

---
+++ src/main/java/com/innowise/orderservice/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.innowise.orderservice.repository;
+
+import com.innowise.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
+
+  
+}
---
+++ src/main/java/com/innowise/orderservice/specification/OrderSpecification.java
@@ -0,0 +1,37 @@
+package com.innowise.orderservice.specification;
+
+import com.innowise.orderservice.entity.Order;
+import com.innowise.orderservice.entity.enums.Status;
+import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.Specification;
+
+public class OrderSpecification {
+
+  private OrderSpecification() {
+
+  }
+
+  public static Specification<Order> hasStatus(Status status) {
+    return (((root, query, criteriaBuilder) ->
+        status == null ? null : criteriaBuilder.equal(root.get("status"), status))
+    );
+  }
+
+  public static Specification<Order> hasFromDate(LocalDateTime from) {
+    return (((root, query, criteriaBuilder) ->
+        from == null ? null : criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), from))
+    );
+  }
+
+  public static Specification<Order> hasToDate(LocalDateTime to) {
+    return (((root, query, criteriaBuilder) ->
+        to == null ? null : criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), to))
+    );
+  }
+
+  public static Specification<Order> isNotDeleted() {
+    return (((root, query, criteriaBuilder) ->
+        criteriaBuilder.equal(root.get("deleted"), false))
+    );
+  }
+}
