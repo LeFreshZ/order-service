b45fe7054e2685eab15814c1b6014f1fd1986249
lefreshz <scver1@yandex.ru>
2026-06-11 21:08:00 +0300

    Add controller layer with security

---
+++ src/main/java/com/innowise/orderservice/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.innowise.orderservice.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
